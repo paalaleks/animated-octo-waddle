7b1e9757feca4ad276062a37e258027709e7b577
Paal Aleksander Kaasa <paalaleksanderkaasa@Paals-MacBook-Air.local>
2022-07-28 12:53:22 +0200

    another one commit

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,28 @@
-import Layout from '../components/layout';
-import StrapiClient from '../lib/strapi-client';
+import Layout from "../components/layout";
+import StrapiClient from "../lib/strapi-client";
 
 export default function Home() {
-	return (
-		<Layout>
-			<div className="section">
-				<div className="container intro">
-					<div className="intro-container">
-						<main>
-							<h1>hello world</h1>
-						</main>
-					</div>
-				</div>
-			</div>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <div className="section">
+        <div className="container intro">
+          <div className="intro-container">
+            <main>
+              <h1>hello world</h1>
+            </main>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
-const strapiClient = new StrapiClient();
+// const strapiClient = new StrapiClient();
 
-export const getStaticProps = async () => {
-	const allArticles = await strapiClient.fetchData('/api/articles');
-	return {
-		props: {
-			articles: allArticles,
-		},
-	};
-};
+// export const getStaticProps = async () => {
+// 	const allArticles = await strapiClient.fetchData('/api/articles');
+// 	return {
+// 		props: {
+// 			articles: allArticles,
+// 		},
+// 	};
+// };
